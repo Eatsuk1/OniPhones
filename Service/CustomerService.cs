a75e0644a9c053d5e9297aa8ea435e73b30c3aac
Anh Lee <anh.lq192684@sis.hust.edu.vn>
2022-06-17 22:54:48 +0700

    update 17/6: update bla bla

--- Service/CustomerService.cs
+++ Service/CustomerService.cs
@@ -1,10 +1,12 @@
-﻿using DoAn1.Data;
+﻿using System;
+using System.Collections.Generic;
+using DoAn1.Data;
 using Microsoft.AspNetCore.Components.Authorization;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using System.Security.Claims;
 using System.Threading.Tasks;
-
+using MongoDB.Bson;
 
 
 namespace DoAn1.Service
@@ -21,7 +23,31 @@ namespace DoAn1.Service
             _collection = db.GetConnection().GetCollection<Customer>("customers");
         }
 
-       
+        public bool VerifyCustomer(string customerid)
+        {
+            var filter = Builders<Customer>.Filter.Eq(x => x.UserId, customerid);
+            if (_collection.Find(filter).FirstOrDefault() != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task AddCustomer(string _customerid, string _name, string _email)
+        {
+            var document = new Customer();
+            document.UserId = _customerid;
+            document.Name = _name;
+            document.Email = _email;
+            document.Birthday = "";
+            document.JoinedDay = DateTime.Today.ToShortDateString();
+            document.PhoneNumber = "";
+            document.address = new List<string>() { "" };
+            await _collection.InsertOneAsync(document);
+        }
 
         public Customer GetCustomer(string Customerid)
         {
@@ -29,8 +55,6 @@ namespace DoAn1.Service
             var filter = Builders<Customer>.Filter.Eq(x => x.UserId, Customerid);
             var projection = Builders<Customer>.Projection.Include("name").Include("joined day");
             return BsonSerializer.Deserialize<Customer>(_collection.Find(filter).Project(projection).FirstOrDefault());
-
-
         }
 
         public Customer GetCustomerAddress(string customerid)
