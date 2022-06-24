3b6bffba73ef65bef6086500f7d897bc87a2378c
Anh Lee <anh.lq192684@sis.hust.edu.vn>
2022-06-24 15:15:23 +0700

    change to update-preview from now

--- Service/ProductService.cs
+++ Service/ProductService.cs
@@ -19,12 +19,12 @@ namespace DoAn1.Service
         public List<Smartphone> GetSmartphones()
         {
 
-            return _collection.Find(_ => true).ToList();
+            return _collection.Find(_ => true).Limit(5).ToList();
         }
 
         public List<Smartphone> GetSmartphonesFilter(string _key)
         {
-            var filter = Builders<Smartphone>.Filter.Regex("key", new BsonRegularExpression(_key));
+            var filter = Builders<Smartphone>.Filter.Regex("key", new BsonRegularExpression(_key, "i"));
             return _collection.Find(filter).ToList();
         }
 
