41e2ddb86e848e042c294c5578bd8d3daf603e46
Anh Lee <anh.lq192684@sis.hust.edu.vn>
2022-06-29 21:38:21 +0700

    update delete testCart

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -1,11 +1,10 @@
 ﻿using DoAn1.Data;
-using System.Collections.Generic;
 
 namespace DoAn1.Models
 {
     public class Cart
     {
-        public Smartphone _Smartphone { get; set; }
+        public Smartphone Smartphone { get; set; }
 
         public int quantity { get; set; }
 
