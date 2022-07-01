0f3bb9b6f77f37058e2e1228582227951917b259
Hiếu Lê <letrunghieu25031208@gmail.com>
2022-07-01 23:01:00 +0700

    fix_all

---
+++ Service/SubHeaderService.cs
@@ -0,0 +1,29 @@
+﻿using DoAn1.Data;
+using DoAn1.Models;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace DoAn1.Service
+{
+    public class SubHeaderService
+    {
+        ClassDB db = new ClassDB();
+        private IMongoCollection<SubHeader> _collection;
+
+        public SubHeaderService()
+        {
+            _collection = db.GetConnection().GetCollection<SubHeader>("category");
+        }
+
+        public SubHeader GetSubHeader(string _device_key)
+        {
+            var filter = Builders<SubHeader>.Filter.AnyEq(x => x.Device_key, _device_key);
+            var projection = Builders<SubHeader>.Projection.Include("Brand").Include("Device_Type").Include("Series");
+            return BsonSerializer.Deserialize<SubHeader>(_collection.Find(filter).Project(projection).FirstOrDefault());
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -49,6 +49,7 @@ namespace DoAn1
             services.AddScoped<Cart>();
             services.AddScoped<List<Cart>>();
             services.AddScoped<CartService>();
+            services.AddScoped<SubHeaderService>();
             //services.AddAuthentication().AddGoogle(googleOptions =>
             //{
             //    googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
