0586c5d436d75577935cc93e0862b2c94dbab345
Anh Lee <anh.lq192684@sis.hust.edu.vn>
2022-07-05 21:56:11 +0700

    update for auth0

--- Startup.cs
+++ Startup.cs
@@ -79,8 +79,7 @@ namespace DoAn1
      options.Scope.Add("profile"); // <- Optional extra
      options.Scope.Add("email");   // <- Optional extra
 
-     options.CallbackPath = new PathString("/verify");
-     options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+     
      options.ClaimsIssuer = "Auth0";
      options.SaveTokens = true;
      options.TokenValidationParameters = new TokenValidationParameters
