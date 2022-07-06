8710e4d426e94afe0f7112f637941eaab19facb0
Anh Lee <anh.lq192684@sis.hust.edu.vn>
2022-07-06 09:53:50 +0700

    update for auth0

---
+++ Pages/Account/Verify.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DoAn1.Pages.Account
+{
+    public class VerifyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
