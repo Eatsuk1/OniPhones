d8da2458a14c807e24c7449fb0b2edb7968b5f8a
Dự Nguyễn Trọng <nguyentrongdupl@gmail.com>
2022-07-11 16:39:05 +0700

    fix add address

--- wwwroot/js/Payment.js
+++ wwwroot/js/Payment.js
@@ -5,12 +5,21 @@
 function createAlert() {
     alert("this is an alert");
 }
-function addPopup() {
-    document.getElementById('addaddress').style.display = 'flex';
-};
+//function addPopup() {
+//    document.getElementById('addaddress').style.display = 'flex';
+//};
 
 
-function addClose() {
-    document.querySelector('.bg-addform').style.display = 'none';
-};
+//function addClose() {
+//    document.querySelector('.bg-addform').style.display = 'none';
+//};
 
+
+
+
+const Showpopup() {
+    const BtnAddress = document.getElementById('addaddress');
+    BtnAddress.onclick = () => {
+        BtnAddress.classList.add('active');
+    }
+}
